3a81668307a5344ae1c389091101994f9a73c012
Paul Blake <pnblake@gmail.com>
2019-04-06 18:22:07 -0400

    update views

--- contract_parser_app/views.py
+++ contract_parser_app/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from .forms import UploadFileForm
 from .parse_script import parse_document
 from django.http import JsonResponse
+from .models import Contract
 
 
 # Create your views here.
@@ -17,11 +18,16 @@ def upload_file(request):
             print ("valid form")
             parsed_doc = parse_document(request.FILES['file'])
             print(parsed_doc)
-            # First:
-            # import model
+            db_doc = Contract.objects.create({
+                'description': parsed_doc['Description'],
+                'background': parsed_doc['BACKGROUND'],
+                'required_services': parsed_doc['REQUIRED SERVICES'],
+                'response_detail': parsed_doc['RESPONSES'],
+                'title': parsed_doc['PWS']
+            })
             # save parsed_doc to DB through model
             # send back instance from model (which will include the ID)
-        return JsonResponse(parsed_doc, safe=False)
+        return JsonResponse(db_doc, safe=False)
         # return render(request, 'contract_parser/contract_show.html', {"contract": parsed_doc})
     else:
         form = UploadFileForm()
