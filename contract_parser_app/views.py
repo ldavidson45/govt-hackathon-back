0a072c4db0bc344b2a67f7ced21bcb47533ccb1c
Liz Davidson <lizdavidson45@gmail.com>
2019-04-06 13:07:27 -0400

    create file upload template and build out sample form

--- contract_parser_app/views.py
+++ contract_parser_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .forms import UploadFileForm
+from .parse_script import parse_document
 
 # Create your views here.
+
+
+# accept a word document and return data 
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            parse_document(request.FILES['file'])
+            return HttpResponseRedirect('/success/url/')
+    else:
+        form = UploadFileForm()
+    return render(request, 'upload.html', {'form': form})
